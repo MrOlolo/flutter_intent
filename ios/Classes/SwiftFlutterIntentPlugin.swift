26c8e58336234ffed5e2018b5e20d9dd62280d99
MrOlolo <MrOlolo@users.noreply.github.com>
2020-01-29 16:29:48 +0300

    Update SwiftFlutterIntentPlugin.swift
    add ios stub

--- ios/Classes/SwiftFlutterIntentPlugin.swift
+++ ios/Classes/SwiftFlutterIntentPlugin.swift
@@ -3,12 +3,12 @@ import UIKit
 
 public class SwiftFlutterIntentPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_intent", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "com.mrololo/flutter_intent", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterIntentPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    result(FlutterMethodNotImplemented)
   }
 }
